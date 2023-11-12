d14ec6e4e63d6ce3766c611a39c48947c3e504e0
Shadowman405 <maximus405@gmail.com>
2023-11-12 20:00:57 +0300

    Initial Commit

---
+++ MTG_all_swiftUI/MTG_all_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTG_all_swiftUIApp.swift
+//  MTG_all_swiftUI
+//
+//  Created by Maxim Mitin on 12.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MTG_all_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
