e9a7bec07c3bfc8a00c95b49e60ebc4be32e5009
Shadowman405 <maximus405@gmail.com>
2023-11-13 15:16:37 +0300

    add network layer

--- MTG_all_swiftUI/Helpers/NetworkManager.swift
+++ MTG_all_swiftUI/Helpers/NetworkManager.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case badUrl
+    case invaledRequest
+    case badResponse
+    case badStatus
+    case failedToDecodeResponse
+}
+
+class WebService{
+    func downloadData<T: Codable>(fromURL: String) async -> T? {
+        do {
+            guard let url = URL(string: fromURL) else { throw NetworkError.badUrl}
+            let (data,response) = try await URLSession.shared.data(from: url)
+            guard let response = response as? HTTPURLResponse else {throw NetworkError.badResponse}
+            guard response.statusCode >= 200 && response.statusCode < 300 else { throw NetworkError.badStatus}
+            guard let decodedResponse = try? JSONDecoder().decode(T.self, from: data) else {throw NetworkError.failedToDecodeResponse}
+            
+            return decodedResponse
+            
+        } catch NetworkError.badUrl {
+            print("Problem with URL")
+        } catch NetworkError.badResponse {
+            print("bad response")
+        } catch NetworkError.badStatus {
+            print("Bad status")
+        } catch NetworkError.failedToDecodeResponse{
+            print("Cant decode data")
+        } catch {
+            print("failure")
+        }
+        
+        return nil
+    }
+}
+
+
