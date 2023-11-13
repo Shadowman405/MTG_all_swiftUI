0064282a12342e14feba660f003b4814d9cef046
Shadowman405 <maximus405@gmail.com>
2023-11-13 11:01:14 +0300

    rebuild view with ScrollView instead of List

--- MTG_all_swiftUI/CardsView.swift
+++ MTG_all_swiftUI/CardsView.swift
@@ -10,18 +10,24 @@ import SwiftUI
 struct CardsView: View {
     @State private var cards = [Card(name: "Abzan Falconer", manaCost: "{2}{W}", cmc: 3, colors: ["W"], colorIdentity: ["W"], type: "Creature — Human Soldier", types: ["Creature"], subtypes: ["Human", "Soldier"], rarity: "Uncommon", setCode: "2X2", setName: "Double Masters 2022", text: "Beep", flavor: "", artist: "", number: "", power: "", toughness: "", layout: "", multiverseid: "", imageURL: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=571337&type=card", printings: [""], originalText: "", originalType: "", legalities: [LegalityElement(format: "Commander", legality: .legal)], id: "18468b64-37ef-5e4d-b95a-781265b533a2"),Card(name: "Doomed Traveler", manaCost: "{W}", cmc: 1, colors: ["W"], colorIdentity: ["W"], type: "Creature — Human Soldier", types: [""], subtypes: [""], rarity: "Rare", setCode: "2X2", setName: "Double Masters 2022", text: "", flavor: "", artist: "", number: "", power: "", toughness: "", layout: "", multiverseid: "", imageURL: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=571342&type=card", printings: [""], originalText: "", originalType: "", legalities: [LegalityElement(format: "Commander", legality: .legal)], id: "270cb060-9fcf-5a5a-b87e-6ec5a0e278b5")]
     
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        List{
-            ForEach(cards) {card in
-                VStack {
-                    AsyncImage(url: URL(string: card.imageURL))
-                        .frame(width: 350, height: 350)
-                        .padding()
-                    //Text(card.name)
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(cards) { card in
+                    AsyncImage(url: URL(string: card.imageURL)) { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                                .padding()
+                    } placeholder: {
+                        ProgressView()
+                    }
+
                 }
             }
         }
-        
+        .navigationTitle("Cards")
     }
 }
 
