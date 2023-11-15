dec4eaf3321b191fa87b3400d538bcf8c90cb88c
Shadowman405 <maximus405@gmail.com>
2023-11-15 15:38:27 +0300

    login view UI configuration - done

--- MTG_all_swiftUI/Views/LoginView.swift
+++ MTG_all_swiftUI/Views/LoginView.swift
@@ -8,21 +8,74 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         ZStack {
             Color(.black)
+                .ignoresSafeArea()
             
             VStack{
-                Image("mtg_login_logo-removebg")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .offset(y: -150)
+                VStack {
+                    Image("mtg_login_logo-removebg")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .offset(y: -50)
+                }
                 
-                Text("Email")
-                    .foregroundColor(.white)
-                    .font(.system(size: 40, weight: .bold, design: .rounded))
-                    .offset(x: -100,y: -100)
+                VStack(alignment: .leading) {
+                    Text("Email")
+                        .foregroundColor(.white)
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                    
+                    TextField("", text: $email, prompt: Text("Email...")
+                        .foregroundColor(.orange.opacity(0.4)))
+                    .textFieldStyle(.plain)
+                    .foregroundColor(.blue)
+                    
+                    Divider()
+                        .foregroundColor(.gray)
+                    
+                    
+                    Text("Password")
+                        .foregroundColor(.white)
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                    
+                    TextField("", text: $password, prompt: Text("Password...")
+                        .foregroundColor(.orange.opacity(0.4)))
+                    .textFieldStyle(.plain)
+                    .foregroundColor(.blue)
+                    
+                    Divider()
+                        .foregroundColor(.gray)
+                    
+                }
+                .padding()
+                
+                VStack {
+                    Button {
+                        
+                    } label: {
+                        Text("Sign Up")
+                            .bold()
+                            .frame(width: 300, height: 40)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                    .fill(LinearGradient(colors: [.brown, .orange], startPoint: .top, endPoint: .bottomTrailing))
+                            )
+                            .foregroundColor(.white)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Already have an account? Login")
+                    }
+                    .offset(y: 10)
+                }
             }
+            .navigationTitle("Login")
         }
     }
 }
