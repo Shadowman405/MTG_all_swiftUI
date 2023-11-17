95525c31714aff3338cf40df245356f07a34876f
Shadowman405 <maximus405@gmail.com>
2023-11-17 14:04:23 +0300

    clean few lines

--- MTG_all_swiftUI/Views/CardsViews/CardManaCostView.swift
+++ MTG_all_swiftUI/Views/CardsViews/CardManaCostView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CardManaCostView: View {
     var card = Card(name: "Abzan Falconer", manaCost: "{1}{B}{B}{B}", cmc: 3, colors: ["W"], colorIdentity: ["W"], type: "Creature — Human Soldier", types: ["Creature"], subtypes: ["Human", "Soldier"], rarity: "Uncommon", setCode: "2X2", setName: "Double Masters 2022", text: "Beep", flavor: "", artist: "", number: "", power: "", toughness: "", layout: "", multiverseid: "", imageURL: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=571337&type=card", printings: [""], originalText: "", originalType: "", legalities: [LegalityElement(format: "Commander", legality: .legal)], id: "18468b64-37ef-5e4d-b95a-781265b533a2")
-    @State var imagesArr = ["{W}"]
+    @State var imagesArr = [""]
     
     var body: some View {
         ForEach(0..<imagesArr.count, id: \.self) { imageName in
@@ -18,18 +18,17 @@ struct CardManaCostView: View {
                 .frame(width: 20, height: 20)
         }
         .onAppear(perform: {
-            imagesArr = splitString(card.manaCost ?? "{W}")
+            imagesArr = splitString(card.manaCost ?? "")
         })
     }
     
     func splitString(_ str: String) -> [String] {
         var newStrArr = [String]()
-        var items = str.split(separator: "}", omittingEmptySubsequences: true)
+        let items = str.split(separator: "}", omittingEmptySubsequences: true)
         for item in items {
             let newStr = item + "}"
             newStrArr.append(String(newStr))
         }
-        print(newStrArr)
         return newStrArr
     }
 }
