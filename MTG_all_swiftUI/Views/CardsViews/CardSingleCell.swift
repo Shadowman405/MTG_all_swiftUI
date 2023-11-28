95b076db542a980aca61dab67474de8448db227a
Shadowman405 <maximus405@gmail.com>
2023-11-28 13:55:54 +0300

    trying to add animation frame to cards

--- MTG_all_swiftUI/Views/CardsViews/CardSingleCell.swift
+++ MTG_all_swiftUI/Views/CardsViews/CardSingleCell.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CardSingleCell: View {
     var card : Card
+    @State private var rotation:CGFloat = 0.0
     
     var body: some View {
 //        AsyncImage(url: URL(string: card.imageURL ?? "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=571337&type=card")) { image in
@@ -25,7 +26,16 @@ struct CardSingleCell: View {
         ZStack {
             RoundedRectangle(cornerSize: CGSize(width: 20, height: 10), style: .circular)
                 .frame(height: 100)
-                .padding()
+                //.padding()
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .frame(height: 100)
+                .foregroundStyle(LinearGradient(colors: [Color(.orange), Color(.purple)], startPoint: .top, endPoint: .bottom))
+                .rotationEffect(.degrees(rotation))
+                .mask {
+                    RoundedRectangle(cornerRadius: 20,style: .continuous)
+                        .stroke(lineWidth: 3)
+                        .frame()
+                }
             
             ZStack {
                 HStack {
@@ -41,6 +51,11 @@ struct CardSingleCell: View {
                 
             }
         }
+        .onAppear(perform: {
+            withAnimation(.linear(duration: 4).repeatForever(autoreverses: false)) {
+                rotation = 360
+            }
+        })
     }
 }
 
