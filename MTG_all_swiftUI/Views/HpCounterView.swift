f764bfe6cff09c770aee0f0db1ddb92b7bb9a439
Shadowman405 <maximus405@gmail.com>
2023-11-29 15:22:35 +0300

    working on HP view

--- MTG_all_swiftUI/Views/HpCounterView.swift
+++ MTG_all_swiftUI/Views/HpCounterView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct HpCounterView: View {
+    @State private var rotation:CGFloat = 0.0
+    
     var body: some View {
         HStack {
-            Text("HP Counter")
-            Image("B")
-                .resizable()
-                .frame(width: 40, height: 40)
+            Button {
+                print("HP")
+            } label: {
+                ZStack {
+                    Image("{B}")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .background(Color.orange)
+                    .clipShape(Circle())
+                    Circle()
+                        .frame(width: 50, height: 50)
+                        .foregroundStyle(LinearGradient(colors: [Color(.orange), Color(.purple)], startPoint: .top, endPoint: .bottom))
+                        .rotationEffect(.degrees(rotation))
+                        .mask {
+                            Circle()
+                                .stroke(lineWidth: 3)
+                        }
+                }
+            }
         }
     }
 }
