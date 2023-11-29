0dd7d1c1c882d0c122ea4f30058e12f10173794e
Shadowman405 <maximus405@gmail.com>
2023-11-29 15:41:30 +0300

    Dont understand why button moves verticly insted of change degrees

--- MTG_all_swiftUI/Views/HpCounterView.swift
+++ MTG_all_swiftUI/Views/HpCounterView.swift
@@ -11,26 +11,28 @@ struct HpCounterView: View {
     @State private var rotation:CGFloat = 0.0
     
     var body: some View {
-        HStack {
+        ZStack {
             Button {
                 print("HP")
             } label: {
                 ZStack {
-                    Image("{B}")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .background(Color.orange)
-                    .clipShape(Circle())
-                    Circle()
-                        .frame(width: 50, height: 50)
+                    RoundedRectangle(cornerSize: CGSize(width: 20, height: 10), style: .circular)
+                        .frame(height: 100)
+                    //.padding()
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .frame(height: 100)
                         .foregroundStyle(LinearGradient(colors: [Color(.orange), Color(.purple)], startPoint: .top, endPoint: .bottom))
                         .rotationEffect(.degrees(rotation))
                         .mask {
-                            Circle()
-                                .stroke(lineWidth: 3)
-                        }
+                            RoundedRectangle(cornerRadius: 20,style: .continuous)
+                            .stroke(lineWidth: 3)                }
                 }
             }
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 4).repeatForever(autoreverses: false)) {
+                    rotation = 360
+                }
+            })
         }
     }
 }
