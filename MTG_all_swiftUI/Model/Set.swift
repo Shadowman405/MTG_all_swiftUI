c9b4e42cedb0a34be1aaf1668b81d2ad7fb755ec
Shadowman405 <maximus405@gmail.com>
2023-12-04 14:25:59 +0300

    add new model for fetching Sets

---
+++ MTG_all_swiftUI/Model/Set.swift
@@ -0,0 +1,26 @@
+//
+//  Set.swift
+//  MTG_all_swiftUI
+//
+//  Created by Maxim Mitin on 4.12.23.
+//
+
+import Foundation
+
+struct SetsMTG: Codable {
+    let sets: [Set]
+}
+
+// MARK: - Set
+struct Set: Codable {
+    let code, name, type: String
+    let booster: [Booster]
+    let releaseDate, block: String
+    let onlineOnly: Bool
+}
+
+enum Booster: String, Codable {
+    case common = "common"
+    case rare = "rare"
+    case uncommon = "uncommon"
+}
