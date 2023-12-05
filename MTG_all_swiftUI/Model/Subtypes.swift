b0ccdd2aedd72dff52edfac3a779ac567c3ff10d
Shadowman405 <maximus405@gmail.com>
2023-12-05 14:35:13 +0300

    add new model for subtypes

---
+++ MTG_all_swiftUI/Model/Subtypes.swift
@@ -0,0 +1,13 @@
+//
+//  Subtypes.swift
+//  MTG_all_swiftUI
+//
+//  Created by Maxim Mitin on 5.12.23.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let subtypes: [String]
+}
