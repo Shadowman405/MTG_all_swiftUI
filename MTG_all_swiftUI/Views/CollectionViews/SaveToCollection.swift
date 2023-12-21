9c861674655977bfcb05321958b217eff6839f0a
Shadowman405 <maximus405@gmail.com>
2023-12-21 15:32:03 +0300

    almost done  - works fine(need to switch toggle after 1 sec

--- MTG_all_swiftUI/Views/CollectionViews/SaveToCollection.swift
+++ MTG_all_swiftUI/Views/CollectionViews/SaveToCollection.swift
@@ -10,31 +10,41 @@ import SwiftUI
 struct SaveToCollection: View {
     //@State private var collections: [Collection] = []
     @State private var showAlert = false
+    @State private var showNotification = false
     @ObservedObject private var vm = CardViewModel()
     @State private var collectionName = ""
     @State private var selection: String?
     var card = Card(name: "Abzan Falconer", manaCost: "{2}{W}", cmc: 3, colors: ["W"], colorIdentity: ["W"], type: "Creature — Human Soldier", types: ["Creature"], subtypes: ["Human", "Soldier"], rarity: "Uncommon", setCode: "2X2", setName: "Double Masters 2022", text: "Beep", flavor: "", artist: "", number: "", power: "", toughness: "", layout: "", multiverseid: "", imageURL: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=571337&type=card", printings: [""], originalText: "", originalType: "", id: "18468b64-37ef-5e4d-b95a-781265b533a2", uuid: "")
     
     var body: some View {
-        List(vm.collectionData, selection: $selection) { collection in
-                HStack {
-                    Text(collection.name)
-                    Spacer()
-                    Button(action: {
-                        let impactMed = UIImpactFeedbackGenerator(style: .heavy)
-                        impactMed.impactOccurred()
-                        
-                        saveToCollection(collection: collection, card: card)
-                    }, label: {
-                        Image(systemName: "plus.circle")
-                            .foregroundColor(.green)
-                    })
-                }
-        }
-        .navigationTitle("Save to collection")
-        .onAppear(perform: {
-            vm.fetchCollectionFromDB()
+        ZStack {
+            List(vm.collectionData, selection: $selection) { collection in
+                    HStack {
+                        Text(collection.name)
+                        Spacer()
+                        Button(action: {
+                            let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+                            impactMed.impactOccurred()
+                            
+                            saveToCollection(collection: collection, card: card)
+                            withAnimation {
+                                showNotification.toggle()
+                            }
+                        }, label: {
+                            Image(systemName: "plus.circle")
+                                .foregroundColor(.green)
+                        })
+                    }
+            }
+            .navigationTitle("Save to collection")
+            .onAppear(perform: {
+                vm.fetchCollectionFromDB()
         })
+            
+            if showNotification {
+                NotifactionView(cardName: card.name ?? "", collectionName: collectionName)
+            }
+        }
     }
     
     func saveToCollection(collection: Collection, card: Card) {
