60e935f04ccdb072535600d577081a2844a5d5e7
Shadowman405 <maximus405@gmail.com>
2024-02-10 17:53:09 +0300

    basic configuration for reg view

--- MTG_all_swiftUI/Views/RegisterView.swift
+++ MTG_all_swiftUI/Views/RegisterView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Email")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                
+                TextField("", text: $email, prompt: Text("Enter Email...")
+                    .foregroundColor(.orange.opacity(0.4)))
+                .textFieldStyle(.plain)
+                .foregroundColor(.blue)
+                .padding()
+                
+                //
+                
+                Text("Password")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                
+                TextField("", text: $password, prompt: Text("Enter Password...")
+                    .foregroundColor(.orange.opacity(0.4)))
+                .textFieldStyle(.plain)
+                .foregroundColor(.blue)
+                .padding()
+                
+                Button {
+                    print("beep")
+                } label: {
+                    Text("Sign Up")
+                        .bold()
+                        .frame(width: 300, height: 40)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(LinearGradient(colors: [.brown, .orange], startPoint: .top, endPoint: .bottomTrailing))
+                        )
+                        .foregroundColor(.white)
+                }
+            }
+        }
     }
 }
 
