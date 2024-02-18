25123181a9961cae2e763be4d3a03e8ea5470cc4
Shadowman405 <maximus405@gmail.com>
2024-02-18 18:37:01 +0300

    add new subview - main idea, create subview and then reuse it up to 4 times in HP view and split main HP view propotianally

---
+++ MTG_all_swiftUI/HPView/HPsubview.swift
@@ -0,0 +1,18 @@
+//
+//  HPsubview.swift
+//  MTG_all_swiftUI
+//
+//  Created by Maxim Mitin on 18.02.24.
+//
+
+import SwiftUI
+
+struct HPsubview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HPsubview()
+}
