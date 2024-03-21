2f34c707bf8ef5d26c86fcff9dbd6dcf3716c0b9
Maxim Mitin <maximus405@gmail.com>
2024-03-21 17:41:43 +0300

    preparing new view

--- MTG_all_swiftUI/Views/CardsViews/AddOwnCardView.swift
+++ MTG_all_swiftUI/Views/CardsViews/AddOwnCardView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct AddOwnCardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .ignoresSafeArea()
+        .navigationTitle("Add your own card")
     }
 }
 
