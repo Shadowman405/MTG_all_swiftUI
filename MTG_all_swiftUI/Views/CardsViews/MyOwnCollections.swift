eefebd23777ffa70150055dfdc50ee764672765f
Maxim Mitin <maximus405@gmail.com>
2024-03-22 16:23:56 +0300

    for some reason nav bar didnt appear in Own Collection View

--- MTG_all_swiftUI/Views/CardsViews/MyOwnCollections.swift
+++ MTG_all_swiftUI/Views/CardsViews/MyOwnCollections.swift
@@ -16,43 +16,45 @@ struct MyOwnCollections: View {
     @State private var collectionName = ""
     
     var body: some View {
-        ForEach(vm.myOwnCollectionData) { collection in
-            NavigationLink {
-                CardsInCollectionView(collectionName: collection.name)
-            } label: {
-                HStack {
-                    Text(collection.name)
-                        .font(.custom(
+        List{
+            ForEach(vm.myOwnCollectionData) { collection in
+                NavigationLink {
+                    CardsInCollectionView(collectionName: collection.name)
+                } label: {
+                    HStack {
+                        Text(collection.name)
+                            .font(.custom(
                                 "AmericanTypewriter",
                                 fixedSize: 16))
-                        .foregroundColor(.orange)
+                            .foregroundColor(.orange)
+                    }
                 }
             }
-        }
-        .onDelete(perform: { indexSet in
-            delete(at: indexSet)
-        })
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button {
-                    print("Add collection")
-                    showAlert = true
-                } label: {
-                    Label("Add", systemImage: "plus.circle")
-                        .foregroundColor(.orange)
-                }
-                .alert(Text("Add New Collection"), isPresented: $showAlert) {
-                    TextField("Collection name",text: $collectionName)
-                        .textInputAutocapitalization(.never)
-                    Button("Save"){vm.saveOwnCollection(collectionName: collectionName)}
-                    Button("Cancel", role: .cancel) {}
+            .onDelete(perform: { indexSet in
+                delete(at: indexSet)
+            })
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        print("Add collection")
+                        showAlert = true
+                    } label: {
+                        Label("Add", systemImage: "plus.circle")
+                            .foregroundColor(.orange)
+                    }
+                    .alert(Text("Add New Collection"), isPresented: $showAlert) {
+                        TextField("Collection name",text: $collectionName)
+                            .textInputAutocapitalization(.never)
+                        Button("Save"){vm.saveOwnCollection(collectionName: collectionName)}
+                        Button("Cancel", role: .cancel) {}
+                    }
                 }
             }
+            .navigationTitle("Collections")
+            .onAppear(perform: {
+                vm.fetchOwnCollectionFromDB()
+            })
         }
-        .navigationTitle("Collections")
-        .onAppear(perform: {
-            vm.fetchOwnCollectionFromDB()
-        })
     }
     
     func delete(at offsets: IndexSet) {
